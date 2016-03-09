1b26d8b4c4ca67ca7c2abae9fc945ca3023c7d5b
erdevepi <pierre.erdeven@ensta-bretagne.org>
2016-03-09 18:05:59 +0100

    Update animaux.py

--- classes_eco/animaux.py
+++ classes_eco/animaux.py
@@ -2,6 +2,7 @@
 # -*- coding: Utf-8 -*-
 
 import sys
+import numpy as np
 from abc import abstractmethod, ABCMeta
 
 
@@ -35,15 +36,137 @@ class Animal(metaclass=ABCMeta):
 		"""
 		pass
 
+	@abstractmethod
+	def bouger(self, animal):
+		pass
+
+	@abstractmethod
+	def manger(self, animal):
+		pass
+
+	
+	def comportement_normal(self, animal):
+		pass
+
+	@abstractmethod
+	def boire(self):
+		pass
+
+	def fuite(self, animal):
+		s_echapper = np.random.rand()
+		# L'animal a une chance sur deux de s'enfuir.
+		if s_echapper < 0.5:
+			# L'animal réussit à s'échapper.
+			self.comportement_normal()
+
+	def a_faim(self):
+		if self.nourriture == 0:
+			print("J'ai faim !")
+
+		#rajouter si nourriture dans le champ de vision -> se déplacer vers elle
+
+	def a_soif(self):
+		if self.eau == 0:
+			print("J'ai soif !")
+
+		#rajouter si eau dans le champ de vision -> se déplacer vers elle
+
+	def champ_de_vision(self):
+		pass
+
+
+	def detecte_nourriture(self):
+		pass
+
+
+
 class Herbivore(Animal):
+	"""
+	Animal grégaire quand il n'est pas en recherche de nourriture. Il s'enfuit à l'approche d'un prédateur.
+	Détection d'un prédateur : probabilité inversemement proportionnelle à la distance du prédateur.
+	Animal possédant un comportement décrit par un automate.
+	"""
+
 	def __init__(self):
 		self.vie = 80
 		self.rapidite = 10
+		self.vision = (5,10)
+		self.quota_nourriture = 5
+		self.quota_eau = 3
+
+	def calcVie(self):
+		self.vie -= 1
+
+	def bouger(self):
+		pass
+
+	def manger(self):
+		while nourriture != quota_nourriture:
+			if self.detecte_predateur():
+				Animal.fuite(herbivore)
+				break
+			else:
+				nourriture += 1
+
+	def boire(self):
+		while eau != quota_eau:
+			if self.detecte_predateur():
+				Animal.fuite(herbivore)
+				break
+			else:
+				eau += 1
+
+	def detecte_predateur(self):
+		# prédateur dans le champ de vision 
+		pass
+
+	def action(self, ressource, ):
+		"""
+		Cas où l'herbivore sera contraint de changer de comportement.
+		"""
+		self.bouger()
+		if self.detecte_predateur():
+			# Prédateur détecté : nouveau comportement : fuite.
+			Animal.fuite()
+		elif Animal.a_faim(self):
+			# Herbivore affamé : chercher de la nourriture.
+			self.manger()
+		elif Animal.a_soif(self):
+			# Herbivore assoiffé : chercher de l'eau.
+			self.boire()
+
+
+
+		print("je mange")
+		ressource[44][27].reduction()
+
+
+
+
+
+
+
+
+class Predateur(Animal):
+	def __init__(self):
+		self.vie = 30
+		self.rapidite = 40
 		self.vision = (5,10)
 
 	def calcVie(self):
 		self.vie -= 1
 
 	def action(self, ressource):
-		print("je mange")
-		ressource[44][27].reduction()
+		pass
+
+class Charognard(Animal):
+	def __init__(self):
+		self.vie = 30
+		self.rapidite = 30
+		self.vision = (5,10)
+
+	def calcVie(self):
+		self.vie -= 1
+
+	def action(self,ressource):
+		pass
