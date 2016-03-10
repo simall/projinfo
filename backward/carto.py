d30ac9a75f390ed18248f912dc32a9dd50855e15
simall <s.allenic@laposte.net>
2016-03-10 19:41:03 +0100

    Update carto.py

--- backward/carto.py
+++ backward/carto.py
@@ -1,27 +1,29 @@
 import constantes
 from ressources import *
 
-class Carto(list):
+class Carto():
 	def __init__(self):
-		carte = open(constantes.file_carte)
+		fichier = open(constantes.file_carte)
 		num_line = 0
 
-		for line in carte:
+		self.carte = []
+
+		for line in fichier:
 			num_col = 0
-			self.append([])
+			self.carte.append([])
 			line = line.strip()
 
 			for number in line:
 				number = int(number)
 
 				if number == 0:
-					self[num_line].append(Terre((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
+					self.carte[num_line].append(Terre((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
 				elif number == 1:
-					self[num_line].append(Herbe((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
+					self.carte[num_line].append(Herbe((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
 				elif number == 2:
-					self[num_line].append(Eau((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
+					self.carte[num_line].append(Eau((num_col*constantes.carre_res[0], num_line*constantes.carre_res[1])))
 
 				num_col += 1
 			num_line += 1
 
-		carte.close()
+		fichier.close()
