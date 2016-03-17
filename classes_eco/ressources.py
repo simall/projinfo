1dbc4f46540c64799b438aaa5b1f9a026f1a2beb
simall <s.allenic@laposte.net>
2016-03-17 18:09:45 +0100

    Update ressources.py

--- classes_eco/ressources.py
+++ classes_eco/ressources.py
@@ -9,9 +9,9 @@ from PyQt5.QtCore import QRectF
 
 #classe abstraite mère
 class Ressource(metaclass=ABCMeta):
-	"""
+	'''
 	Classe abstraite pour les ressources.
-	"""
+	'''
 	cycle_vie = 0
 	__quantite = 0
 	id_res = ''
@@ -19,6 +19,10 @@ class Ressource(metaclass=ABCMeta):
 
 	@abstractmethod
 	def reduction(self):
+		'''
+		Méthode abstraite
+		Détermine la perte de ressource
+		'''
 		pass
 
 	@property
@@ -33,7 +37,15 @@ class Ressource(metaclass=ABCMeta):
 			self.__quantite = 0
 
 class Herbe(Ressource):
+	'''
+	Créé une ressource herbe
+	'''
 	def __init__(self, pos):
+		'''
+		Constructeur
+		Prend en paramêtres:
+		- la position de la ressource sur la carte
+		'''
 		self.cycle_vie = constantes.cyles_par_jour
 		self.quantite = 100
 		self.id_res = 1
@@ -47,7 +59,15 @@ class Herbe(Ressource):
 		return (old-self.quantite)
 
 class Eau(Ressource):
+	'''
+	Créé une ressource eau
+	'''
 	def __init__(self, pos):
+		'''
+		Constructeur
+		Prend en paramêtres:
+		- la position de la ressource sur la carte
+		'''
 		self.cycle_vie = -1#ressource infinie
 		self.quantite = -1
 		self.id_res = 2
@@ -59,7 +79,15 @@ class Eau(Ressource):
 		return constantes.bouchee
 
 class Terre():
+	'''
+	Créé une case de terre
+	'''
 	def __init__(self, pos):
+		'''
+		Constructeur
+		Prend en paramêtres:
+		- la position de la terre sur la carte
+		'''
 		self.id_res = 0
 		self.rect = QRectF(pos[0], pos[1], constantes.carre_res[0], constantes.carre_res[1])
 		self.passable = True
