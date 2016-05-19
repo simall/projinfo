3e5d974af83f5d118aa447a164aadebc72b83fa8
erdevepi <pierre.erdeven@ensta-bretagne.org>
2016-05-19 17:46:55 +0200

    Update carto.py

--- backward/carto.py
+++ backward/carto.py
@@ -15,6 +15,10 @@ class ResMap():
 
 		self.carte = []
 		self.drained_herb = Pile()
+		self.dead_prey = Pile()
+		self.rotten_prey = Pile()
+
+		self.prev_case = 0
 
 		for line in fichier:
 			num_col = 0
@@ -45,16 +49,55 @@ class ResMap():
 			reste = 0
 
 		self.drained_herb.put(i, j, constantes.cycles_regen - reste)
-		(self.carte[i][j]).id_res = -(self.carte[i][j]).id_res#met en état épuisé
+		(self.carte[i][j]).id_res = -(self.carte[i][j]).id_res #met en état épuisé
 
 	def check_regen_state(self):
 		elt = self.drained_herb.read()
 
 		if elt is not None:
 			if elt.nb_tours <= 0:
-				(self.carte[elt.i][elt.j]).id_res = -(self.carte[elt.i][elt.j]).id_res#met en état open
+				(self.carte[elt.i][elt.j]).id_res = -(self.carte[elt.i][elt.j]).id_res #met en état open
 
 				self.drained_herb.pop()
 				self.check_regen_state()
+			else:
+				elt.nb_tours -= 1
+
+
+	def dead(self, i, j):
+		cmpt = self.dead_prey.read_base()
+
+		if cmpt is not None:
+			reste = cmpt.nb_tours
+		else:
+			reste = 0
+
+		self.dead_prey.put(i, j, constantes.cycles_dead - reste)
+		self.prev_case = (self.carte[i][j]).id_res
+		(self.carte[i],[j]).id_res = 3
+
+	def check_dead_state(self):
+		elt = self.dead_prey.read()
+
+		if elt is not None:
+			if elt.nb_tours <= 0:
+				(self.carte[elt.i][elt.j]).id_res = -(self.carte[elt.i][elt.j]).id_res #cadavre périmé
+
+				self.rotten_prey.put(i, j, constantes.cycles_rotten)
+				self.dead_prey.pop()
+				self.check_dead_state()
+			else:
+				elt.nb_tours -= 1
+
+
+	def check_rotten_state(self):
+		elt = self.rotten_prey.read()
+
+		if elt is not None:
+			if elt.nb_tours <= 0:
+				(self.carte[elt.i][elt.j]).id_res = self.prev_case
+
+				self.rotten_prey.pop()
+				self.check_rotten_state()
 			else:
 				elt.nb_tours -= 1
